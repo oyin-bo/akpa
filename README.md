9ad7ff267b4d46264c317a569a5768f3b9cb6e2c
Oleg Mihailik <mihailik@gmail.com>
2024-02-03 16:45:26 +0000

    Basic readme and stream function.

---
+++ README.md
@@ -0,0 +1,8 @@
+![md(`](akpa-icon-128.png)
+
+# &#128092; AsynKronous Primitive Algebra
+
+Library to handle async iterators with some of the features similar
+to RX.js.
+
+<!-- `)] // -->
---
+++ index.js
@@ -0,0 +1,114 @@
+(function (global_this, global_window, global_self, module_withExports) {
+
+  const akpa = {
+    version: '0.0.1',
+    stream
+  };
+
+  if (module_withExports) {
+    module_withExports.exports = akpa;
+  } else {
+    for (const entry in [global_this, global_window, global_self]) {
+      entry.akpa = akpa;
+    }
+  }
+
+  /**
+   * @template T
+   * @param {({ yield, reject, complete, finally }: {
+   *  yield: (item: T) => Promise<void>,
+   *  reject: (error: Error) => void,
+   *  complete: () => void,
+   *  finally: Promise<void>
+   * }) => void } callback
+   */
+  async function* streamBuffer(callback) {
+
+    let finallyTrigger = () => { };
+    let stop = false;
+    let buffer = [];
+
+    let continueTrigger = () => { };
+    let continuePromise = new Promise(resolve => continueTrigger = resolve);
+
+    let yieldCompletedTrigger = () => { };
+    let yieldCompletedPromise = new Promise(resolve => yieldCompletedTrigger = resolve);
+
+    let rejectError;
+
+    callback({
+      yield: yieldFn,
+      reject,
+      complete,
+      finally: new Promise(resolve => finallyTrigger = resolve)
+    });
+
+    try {
+      while (!stop) {
+
+        await continuePromise;
+        if (rejectError)
+          throw rejectError.error;
+        if (stop) return;
+
+        continuePromise = new Promise(resolve => continueTrigger = resolve);
+        const yieldBuffer = buffer;
+        buffer = [];
+
+        if (yieldBuffer.length > 0)
+          yield yieldBuffer;
+
+        const yieldCompleted = yieldCompletedTrigger;
+        yieldCompletedPromise = new Promise(resolve => yieldCompletedTrigger = resolve);
+
+        yieldCompleted();
+      }
+
+    } finally {
+      finallyTrigger();
+    }
+
+    function yieldFn(item) {
+      if (stop) {
+        console.error('Cannot yield after complete.');
+        return;
+      }
+      if (rejectError) {
+        console.error('Cannot yield after reject.');
+        return;
+      }
+
+      buffer.push(item);
+      continueTrigger();
+
+      return yieldCompletedPromise;
+    }
+
+    function reject(error) {
+      if (stop) {
+        console.error('Cannot reject after complete.');
+        return;
+      }
+      if (rejectError) {
+        console.error('Cannot reject after reject.');
+        return;
+      }
+
+      rejectError = { error };
+    }
+
+    function complete() {
+      stop = true;
+      continueTrigger();
+    }
+
+    function finallyFn() {
+    }
+  }
+
+})(
+  this,
+  typeof window !== 'undefined' && window,
+  typeof self !== 'undefined' && self,
+  typeof module !== undefined && module && module.exports && module
+)
